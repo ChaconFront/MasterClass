f0e37f3271762041571fe2f62a009f238275789c
Pito*02bernal <jorge02chm@gmail.com>
2024-11-21 19:08:16 -0500

    validation_pipes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,15 +8,22 @@ import {
   Body,
   Headers,
   Ip,
+  ParseIntPipe,
+  DefaultValuePipe
 } from '@nestjs/common';
 //import { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id/:optional?')
-  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
-    console.log(id);
-    console.log(limit);
+  @Get('/:id?')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit',new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(typeof id);
+    console.log( limit);
+    console.log( page);
     return 'you sent a get request to users endpoint';
   }
 

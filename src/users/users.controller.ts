1b24222ac933309b4f5895db771f981fab4ce042
Pito*02bernal <jorge02chm@gmail.com>
2024-11-22 12:02:34 -0500

    validationPipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,25 +2,27 @@ import {
   Controller,
   Get,
   Post,
-  Put,
   Param,
   Query,
   Body,
   ParseIntPipe,
   DefaultValuePipe,
+  Patch,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { GetUsersParamDto } from './dto/get-users-params.dto';
+import { PatchUserDto } from './dto/patch-user.dto';
 //import { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id?')
   public getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
+    @Param() getUserParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log(typeof id);
+    console.log(getUserParamDto);
     console.log(limit);
     console.log(page);
     return 'you sent a get request to users endpoint';
@@ -34,12 +36,14 @@ export class UsersController {
 
   @Post()
   public createUsers(@Body() createUserDto: CreateUserDto) {
-    console.log(typeof createUserDto);   
+    console.log(typeof createUserDto);
+    console.log(createUserDto instanceof CreateUserDto);
     return 'you sent a Post  request to users endpoint';
   }
-  
-  @Put()
-  public updateUsers() {
-    return 'you sent a update request to users endpoint';
+
+  @Patch()
+  public patchUser(@Body() patchUserDto: PatchUserDto) {
+    console.log(patchUserDto);
+    return 'you sent a patch request to users endpoint';
   }
 }

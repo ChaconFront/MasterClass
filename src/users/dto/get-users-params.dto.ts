5626556830c61f9968a337f78bec603e86d014b5
Pito*02bernal <jorge02chm@gmail.com>
2024-11-28 15:26:38 -0500

    ApiPropertyOptional

--- src/users/dto/get-users-params.dto.ts
+++ src/users/dto/get-users-params.dto.ts
@@ -1,7 +1,12 @@
 import { IsInt, IsOptional } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class GetUsersParamDto {
+  @ApiPropertyOptional({
+    description:"Get user with specific id",
+    example:1234,
+  })//esta propiedad especifica en swagger que este parametro es opcional
   @IsOptional()
   @IsInt()
   @Type(() => Number)

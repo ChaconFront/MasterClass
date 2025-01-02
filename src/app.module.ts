eb06e96018b19cfa838940705bcfb0ca141d6f42
jlchacon <jorge02chm@gmail.com>
2025-01-02 17:47:14 -0500

    entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,27 @@ import { PostController } from './post/post.controller';
 import { PostModule } from './post/post.module';
 import { PostService } from './post/providers/post.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostModule, AuthModule],
+  imports: [UsersModule, PostModule, AuthModule,
+    TypeOrmModule.forRootAsync({
+      imports:[],
+      inject:[],
+      useFactory: () => ({
+        type: 'postgres',
+        entities:[],
+        synchronize:true,//no usar en produccion.
+        port:5432,
+        username:'postgres',
+        password:'password*',
+        host: 'localhost',
+        database: 'nestjs-blog',
+
+      })
+    
+    })
+  ],
   controllers: [AppController, PostController],
   providers: [AppService, PostService],
 })

fc9ad0d6fb023ec56525cb58e78ec0b28dfffeb4
jlchacon <jorge02chm@gmail.com>
2025-01-14 16:01:58 -0500

    ConfigService and .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,27 +10,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { TagsModule } from './tags/tags.module';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [UsersModule, PostModule, AuthModule,
     ConfigModule.forRoot({
       isGlobal:true,
-      
+      envFilePath:['.env'],
     }),
     TypeOrmModule.forRootAsync({
-      imports:[],
-      inject:[],
-      useFactory: () => ({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory: (configService:ConfigService) => ({
         type: 'postgres',
         //entities:[User],
         autoLoadEntities:true,//no usar en produccion.
         synchronize:true,
-        port:5432,
-        username:'postgres',
-        password:'password*',
-        host: 'localhost',
-        database: 'nestjs-blog',
+        port:configService.get<number>('DATABASE_PORT'),
+        username:configService.get<string>('DATABASE_USER'),
+        password:configService.get<string>('DATABASE_PASSWORD'),
+        host: configService.get<string>('DATABASE_HOST'),
+        database: configService.get<string>('DATABASE_NAME'),
 
       })
     

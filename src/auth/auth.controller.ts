1016df73391709162e58e20441627277cbe0a409
jlchacon <jorge02chm@gmail.com>
2025-02-01 02:11:39 -0500

    endpoint refreshtokens

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { AuthService } from './providers/auth.service';
 import { SignInDto } from './dtos/signin.dto';
 import { Auth } from './decorator/auth.decorator';
 import { AuthType } from './enums/auth-type.enum';
+import { RefreshTokenDto } from './dtos/refresh-token.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -14,6 +15,13 @@ export class AuthController {
   @Auth(AuthType.None)
   public async SingIn(@Body() signIndto:SignInDto){
     return this.authService.SingIn(signIndto)
+  }
+
+  @Post('Refresh-token')
+  @HttpCode(HttpStatus.OK)
+  @Auth(AuthType.None)
+  public async refreshToken(@Body() refreshToken:RefreshTokenDto){
+    return this.authService.refreshTokens(refreshToken)
   }
 
 
--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable, forwardRef, Inject } from '@nestjs/common';
 import { UserService } from 'src/users/providers/users.service';
 import { SignInDto } from '../dtos/signin.dto';
 import { SingInProvider } from './sing-in.provider';
+import { RefreshTokenDto } from '../dtos/refresh-token.dto';
+import { RefreshTokensProviders } from './refresh-tokens.providers';
 
 @Injectable()
 export class AuthService {
@@ -10,13 +12,15 @@ export class AuthService {
     private readonly userService: UserService,
 
     private readonly singInProvider:SingInProvider,
+
+    private readonly genrerateRefreshToken:RefreshTokensProviders
   ) {}
 
   public async SingIn(singIndto:SignInDto) {
     return await this.singInProvider.singIn(singIndto)
   }
 
-  public isAuth() {
-    return true;
-  }
+ public async refreshTokens(refreshTokenDto: RefreshTokenDto){
+    return await this.genrerateRefreshToken.refreshToken(refreshTokenDto)
+ }
 }
